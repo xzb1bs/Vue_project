4353ceadd906380fb1810c2912b7583d2e16c2da
xzb1bs <beibarssagidolla47@gmail.com>
2025-10-21 10:24:37 +0500

    project

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,8 @@
 <template>
   <header class="container header">
-    <div class="logo">Qamqorlyq</div>
+    <div class="logo">
+      <img src="/qamqorlyq.ico" alt="Logo" class="logo-icon">
+      Qamqorlyq</div>
     <nav class="nav">
       <router-link to="/">Главная</router-link>
       <router-link to="/pets">Все питомцы</router-link>
@@ -11,3 +13,17 @@
 
 <script setup>
 </script>
+<style>
+.logo {
+display: flex;
+align-items: center;
+font-weight: 700;
+font-size: 22px;
+gap: 8px;
+}
+.logo-icon {
+width: 28px;
+height: 28px;
+object-fit: contain;
+}
+</style>
